598c81c8795ed528ce6b5e8a64db7b561a40764d
Cleiton Cabral <cleitoncabral177@gmail.com>
2020-11-20 23:01:13 -0300

    Consumindo API

--- src/components/content/input/input.vue
+++ src/components/content/input/input.vue
@@ -3,19 +3,35 @@
     <b-form-group class="input">
       <label for="" class="ml-3">O que já assisti?</label>
       <div class="col-12 form-inline">
-      <b-form-input placeholder="Procurar conteúdo" style="width:70%; outline:none"></b-form-input>
-        <b-button style="background-color:green" class="ml-3" type="submit"><fa-icon :icon="['fas', 'arrow-right']" size="1x"  ></fa-icon></b-button>
+      <b-form-input v-model="search" placeholder="Procurar conteúdo" style="width:70%; outline:none"></b-form-input>
+        <b-button style="background-color:green" class="ml-3" type="submit" @click.enter="searchContent"><fa-icon :icon="['fas', 'arrow-right']" size="1x"  ></fa-icon></b-button>
       </div>
     </b-form-group>
   </section>
 </template>
 
 <script>
+import Axios from 'axios'
 export default {
 components:{
-  
+},
+  data(){
+    return{
+      search:'',
+      contentApi: ''
+    }
+  },
+  methods:{
+    searchContent(){
+    const apiKey = 'c588fadd20c0db71341bc6404a868279'
+      Axios.get(`https://api.themoviedb.org/3/search/multi?api_key=${apiKey}&language=pt-br&query=${this.search}&page=1&include_adult=false`
+      ).then(resp => (
+        this.contentApi = resp.data
+      ))
+    }
+  }
 }
-}
+
 </script>
 
 <style>
