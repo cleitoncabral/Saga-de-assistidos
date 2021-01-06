b75d20bd91ec9fbb8761965d7eb866ca90af1b71
Cleiton Cabral <cleitoncabral177@gmail.com>
2021-01-06 00:28:12 -0300

    test

--- src/components/content/contentWatched.vue
+++ src/components/content/contentWatched.vue
@@ -36,7 +36,7 @@
 
 export default {
     methods:{
-      deleteItem(content.id){
+      deleteItem(content){
         this.$store.state.contentWatched.splice(indexOf(content.id), 1)
       }
     },
